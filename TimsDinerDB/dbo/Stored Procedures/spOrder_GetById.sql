be95b004acc9020151840e4385b1a2ac6075c08a
Apoka5555 <apoka5555@gmail.com>
2020-10-11 02:54:33 +0600

    add files

---
+++ TimsDinerDB/dbo/Stored Procedures/spOrder_GetById.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[spOrder_GetById]
+	@id int
+AS
+BEGIN
+SET NOCOUNT ON;
+
+SELECT [Id], [OrderName], [OrderDate], [FoodId], [Quantity], [Total] FROM dbo.[Order] WHERE Id = @id
+
+END
